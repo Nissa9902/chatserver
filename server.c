03fd17ab9698aba7ded2d9aac767e1a9fe7a3b7e
David Ferry <dferry@slu.edu>
2024-04-21 21:27:15 -0500

    Update server.c

--- server.c
+++ server.c
@@ -7,4 +7,12 @@
 // If reading or writing to a client's socket fails, then that client should be removed
       from the linked list. 
 
-        
+// Remember that blocking read calls will cause your server to stall. Instead, set your
+// your sockets to be non-blocking. Then, your reads will never block, but instead return
+// an error code indicating there was nothing to read- this error code can be either
+// EAGAIN or EWOULDBLOCK, so make sure to check for both. If your read call fails
+// with that error, then ignore it. If it fails with any other error, then treat that
+// client as though they have disconnected.
+
+// You can create non-blocking sockets by passing the SOCK_NONBLOCK argument to both
+// the socket() function, as well as the accept4() function.
